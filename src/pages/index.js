c102bbf1005f6d386ca157f0c391b421b94ec8e2
Lucas Homer <lucas.homer@gmail.com>
2018-08-18 13:40:46 -0500

    rolled part three into the part two project

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,5 +22,12 @@ export default () => (
 		</blockquote>
 		</div>
 		<p>Posted April 09, 2011</p>
+		<div>
+			<h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+			<p>
+			What do I like to do? Lots of course but definitely enjoy building
+			websites.
+			</p>
+		</div>
 	</div>
 );
